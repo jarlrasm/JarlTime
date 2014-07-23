c73dcdb22c97aeab6420b5d7baaa9b5c745d15f5
Jarl Sveinung Flø Rasmussen <jarl@jarlandying.com>
2014-07-23 22:01:51 +0200

    Readme

--- JarlTime/Projections/Gregorian.cs
+++ JarlTime/Projections/Gregorian.cs
@@ -5,15 +5,16 @@ namespace JarlTime.Projections
 	
 	public class Gregorian:IProjection
 	{
-      private readonly Time time;
-      private readonly TimeZone timezone;
-		public Gregorian(ITimeContext context,int year,int month,int day, int hour=0,int minute=0,int second=0, int milliseconds=0,TimeZone timezone=null)
+		private readonly Time time;
+		private readonly TimeZone timezone;
+
+		public Gregorian (ITimeContext context, int year, int month, int day, int hour = 0, int minute = 0, int second = 0, int milliseconds = 0, TimeZone timezone = null)
 		{
 			if (timezone == null)
 				timezone = TimeZone.UTC ();
-         	this.timezone = timezone;
+			this.timezone = timezone;
 			//TODO: Validate input
-			this.time = new DateTime(year, month, day, hour, minute, second, milliseconds, System.DateTimeKind.Utc).ToTime(context);
+			this.time = new DateTime (year, month, day, hour, minute, second, milliseconds, System.DateTimeKind.Utc).ToTime (context);
 
 		}
 
@@ -22,52 +23,54 @@ namespace JarlTime.Projections
 			this.time = time;
 			if (timezone == null)
 				timezone = TimeZone.UTC ();
-         	this.timezone = timezone;
+			this.timezone = timezone;
 		}
 
-		public int Year 
-      {
-         get{return time.ToDateTime(timezone.ToTimeZoneInfo()).Year; }
+		public int Year {
+			get{ return time.ToDateTime (timezone.ToTimeZoneInfo ()).Year; }
 		}
-		public int Month 
-      {
-         get{return time.ToDateTime(timezone.ToTimeZoneInfo()).Month; }
+
+		public int Month {
+			get{ return time.ToDateTime (timezone.ToTimeZoneInfo ()).Month; }
 		}
-		public int Day 
-		{
-         	get
-			{
-				return time.ToDateTime(timezone.ToTimeZoneInfo()).Day; 
+
+		public int Day {
+			get {
+				return time.ToDateTime (timezone.ToTimeZoneInfo ()).Day; 
 			}
 		}
+
 		public int Hour {
-         get{return time.ToDateTime(timezone.ToTimeZoneInfo()).Hour; }
+			get{ return time.ToDateTime (timezone.ToTimeZoneInfo ()).Hour; }
 		}
+
 		public int Minute {
-         get{return time.ToDateTime(timezone.ToTimeZoneInfo()).Minute; }
+			get{ return time.ToDateTime (timezone.ToTimeZoneInfo ()).Minute; }
 		}
+
 		public int Second {
-         get{return time.ToDateTime(timezone.ToTimeZoneInfo()).Second; }
+			get{ return time.ToDateTime (timezone.ToTimeZoneInfo ()).Second; }
 		}
+
 		public int Millisecond {
-         get{return time.ToDateTime(timezone.ToTimeZoneInfo()).Millisecond; }
+			get{ return time.ToDateTime (timezone.ToTimeZoneInfo ()).Millisecond; }
 		}
 
 		public override string ToString ()
 		{
-         return time.ToDateTime(timezone.ToTimeZoneInfo()).ToString();
+			return time.ToDateTime (timezone.ToTimeZoneInfo ()).ToString ();
 		}
 
 
 		public string ToString (string format)
 		{
-         return time.ToDateTime(timezone.ToTimeZoneInfo()).ToString(format);//This will  not be like this in the future.
+			return time.ToDateTime (timezone.ToTimeZoneInfo ()).ToString (format);//This will  not be like this in the future.
 		}
 
 
 		public Time Time {
 			get {
-            return time;
+				return time;
 			}
 		}
 
--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 JarlTime
 ========
 A simple .NET library for time.
+
+Ideas
+-----
+*Separation of logic and presentation: Time struct ignores all information about timezones and calendars.
+*Testability: Inject context to your classes for different meanings of Now
+*Immutability
+*Simplicity?
+
+Examples
+--------
+If you like to inject code:
+```csharp
+ITimeContext timeContext = new TimeContext();
+Time time = timeContext.Now();
+Gregorian gregorian=time.Projection<Gregorian>(timeContext.Here());
+int year=gregorian.Year;
+```
+
+If you are lazy
+```csharp
+Time time = Rigth.Now;
+Gregorian gregorian=time.Projection<Gregorian>(Rigth.Here)
+...
+```
