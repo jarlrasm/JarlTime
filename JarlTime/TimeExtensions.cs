e51969aa21c053bb1d691ec0f0da8c8d0909e4be
Jarl Sveinung Flø Rasmussen <jarl@jarlandying.com>
2014-07-23 21:40:26 +0200

    Formatting

--- JarlTime/TimeExtensions.cs
+++ JarlTime/TimeExtensions.cs
@@ -4,32 +4,36 @@ namespace JarlTime
 {
 	public static class TimeExtensions
 	{
-		public static Interval Interval(this Time time, Time othertime)
+		public static Interval Interval (this Time time, Time othertime)
 		{
-			return new Interval(time.SecondsFromEpoch-othertime.SecondsFromEpoch);
+			return new Interval (time.SecondsFromEpoch - othertime.SecondsFromEpoch);
 		}
-		public static Time Add(this Time time, Interval interval)
+
+		public static Time Add (this Time time, Interval interval)
 		{
-			return new Time (time.SecondsFromEpoch + interval.Seconds,time.Context);
+			return new Time (time.SecondsFromEpoch + interval.Seconds, time.Context);
 		}
-		public static bool IsBefore(this Time time, Time otherTime)
+
+		public static bool IsBefore (this Time time, Time otherTime)
 		{
 			return time < otherTime;
 		}
-		public static bool IsAfter(this Time time, Time otherTime)
+
+		public static bool IsAfter (this Time time, Time otherTime)
 		{
 			return time > otherTime;
 		}
-         public static DateTime ToDateTime (this Time fromtime, TimeZoneInfo timezone)
-         {
-            return TimeZoneInfo.ConvertTime(new DateTime(1970, 1, 1, 0, 0, 0, 0,DateTimeKind.Utc)
-                                         .AddSeconds((double)fromtime.SecondsFromEpoch),TimeZoneInfo.Utc,timezone);
-         }
 
-		public static Time ToTime(this DateTime dateTime,ITimeContext context)
-         {
-			return new Time(new decimal((dateTime.ToUniversalTime()-new DateTime(1970,1,1,0,0,0,0,System.DateTimeKind.Utc)).TotalSeconds),context);
-         }
+		public static DateTime ToDateTime (this Time fromtime, TimeZoneInfo timezone)
+		{
+			return TimeZoneInfo.ConvertTime (new DateTime (1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)
+                                         .AddSeconds ((double)fromtime.SecondsFromEpoch), TimeZoneInfo.Utc, timezone);
+		}
+
+		public static Time ToTime (this DateTime dateTime, ITimeContext context)
+		{
+			return new Time (new decimal ((dateTime.ToUniversalTime () - new DateTime (1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc)).TotalSeconds), context);
+		}
 	}
 }
 
--- JarlTime/Timezone.cs
+++ JarlTime/Timezone.cs
@@ -1,26 +1,31 @@
 using System;
 using System.Collections.Generic;
+
 namespace JarlTime
 {
 	public class TimeZone//TODO: Does not really do much yet. It is supposed to implement IANA tz: http://www.iana.org/time-zones
 	{
-        private readonly TimeZoneInfo internalTz;
+		private readonly TimeZoneInfo internalTz;
 
-		private TimeZone(System.TimeZoneInfo tz)
+		private TimeZone (System.TimeZoneInfo tz)
 		{
 			this.internalTz = tz;
 		}
-		public static TimeZone UTC()
+
+		public static TimeZone UTC ()
 		{
-         return new TimeZone ( System.TimeZoneInfo.Utc);
+			return new TimeZone (System.TimeZoneInfo.Utc);
 		}
-        public TimeZoneInfo ToTimeZoneInfo()
+
+		public TimeZoneInfo ToTimeZoneInfo ()
 		{
-            return internalTz;
+			return internalTz;
 		}
-		public  static TimeZone FromTimeZoneInfo(TimeZoneInfo tzInfo)
+
+		public  static TimeZone FromTimeZoneInfo (TimeZoneInfo tzInfo)
 		{
-			return new TimeZone (tzInfo);;
+			return new TimeZone (tzInfo);
+			;
 		}
 	}
 	
