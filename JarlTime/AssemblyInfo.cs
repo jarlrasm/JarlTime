170ab7b249b105b85223b947c27015bfcf883613
Jarl Rasmussen <jarl@okb.no>
2014-07-24 13:10:24 +0200

    Updated assemblyinfo

--- JarlTime/AssemblyInfo.cs
+++ JarlTime/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("JarlTime")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Replacement for DateTime")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jarl Sveinung Rasmussen")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("jarl")]
+[assembly: AssemblyCopyright("Jarl Sveinung Rasmussen")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
