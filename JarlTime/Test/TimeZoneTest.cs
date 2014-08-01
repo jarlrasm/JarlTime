5c11e149bbc76d4d20503a9fafc3bfb1fdb3ee7d
Jarl Rasmussen <jarl@okb.no>
2014-08-01 12:13:17 +0200

    Slight rewriting of tests

--- JarlTime/Test/TimeZoneTest.cs
+++ JarlTime/Test/TimeZoneTest.cs
@@ -16,19 +16,21 @@ namespace Test
         public void TimeZoneGmtIsGmt()
         {
             var timezone = Right.AtGmt;
+            var time = new Time(205956690, TimeContext.Default);
+            var gregorian = time.Projection<Gregorian>(timezone);
             Assert.AreEqual("Etc/GMT", timezone.Name);
             Assert.AreEqual(TimeZoneInfo.Utc, timezone.ToTimeZoneInfo());
+            Assert.AreEqual("11.07.1976 18:11:30", gregorian.ToString());
         }
         [Test]
         public void TimeZoneAtOsloIsAtOslo()
         {
-            var timezone = Right.At("Europe/Oslo");
-            var time = Right.Now;
-            var datetime = time.ToDateTime(TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time"));
+            var timezone = Right.At("Europe/Oslo"); 
+            var time=new Time(205956690, TimeContext.Default);
             var gregorian = time.Projection<Gregorian>(timezone);
-            Assert.AreEqual(datetime.ToString(),gregorian.ToString());
             Assert.AreEqual("Europe/Oslo", timezone.Name);
             Assert.AreEqual(TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time"), timezone.ToTimeZoneInfo());
+            Assert.AreEqual("11.07.1976 20:11:30", gregorian.ToString());
         }
     }
 }
